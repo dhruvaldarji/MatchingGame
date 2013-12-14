bdb9b23346e0f78566b63ec24050f81d12677d6f
dhruvaldarji <dhruvaldarji@gmail.com>
2013-12-13 23:07:52 -0500

    Initial Commit

---
+++ MatchingGame/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  MatchingGame
+//
+//  Created by Dhruval's MacBook on 12/13/13.
+//  Copyright (c) 2013 Dhruval Darji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ MatchingGame/CardGameViewController.m
@@ -0,0 +1,29 @@
+//
+//  CardGameViewController.m
+//  MatchingGame
+//
+//  Created by Dhruval's MacBook on 12/13/13.
+//  Copyright (c) 2013 Dhruval Darji. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface CardGameViewController ()
+
+@end
+
+@implementation CardGameViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
